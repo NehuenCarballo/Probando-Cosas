e60c245b0d24f7fb91962bf6b70d39e6477249e7
NehuenCarballo <nehuencar123@gmail.com>
2024-01-11 20:14:50 -0300

    Modificaciones basicas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { Router, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
-import Categories from './Components/Categories';
-
+import Categories from "./Components/Categories";
 
 export default function App() {
- return (
-  <>
-  <h1>Mapeo</h1>
-  <Router>
-    <Routes>
-      <Route path ='/' element={<Home/>}/>
-      <Route path="/categorias" element={<Categories/>}/>
-    </Routes>
-  </Router>  
-  </>
- );
+  return (
+    <>
+      <h1>Mapeo</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categorias" element={<Categories />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
-
-
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,60 @@
+import { useState } from "react";
+import { AgregarTarea } from "./AgregarTarea"
+ 
+const Item = ({ nombre, visto }) => {
+  return (
+    <li>
+      {nombre}
+      {visto ? "✔" : "💔"}
+    </li>
+  );
+};
+
 export default function Home() {
+  //const [contador, setContador] = useState (500)
+  const addTask = () => {
+    setArreglo([...arreglo, { nombre: 'nuevo', visto: true }]);
+    
+  };
+  let listado = [
+    { nombre: "Instalaciones Necesarias", visto: true },
+    { nombre: "Hooks", visto: true },
+    { nombre: "Props", visto: true },
+    { nombre: "Componentes", visto: false },
+  ];
+  const [arreglo, setArreglo] = useState(listado);
+
+  /*const handleClick = () => {
+     setContador (contador + 1)
+    console.log(num)
+  }*/
+
   return (
     <>
-    <ul>
+      {/* <ul>
         <il>
-            <h1>Home</h1>
+            <h1>Contador</h1>
+               //<p>{contador} </p>
+              //<button onClick={handleClick}> Soy un Boton </button> 
         </il>
-    </ul>
+    </ul> */}
+
+      <h1>Mapeo</h1>
+
+      <AgregarTarea agregarTarea={setArreglo}></AgregarTarea>
+
+      <ol>
+        {arreglo.map((item) => (
+          <Item
+            key={item.nombre}
+            nombre={item.nombre}
+            visto={item.visto}
+          ></Item>
+        ))}
+      </ol>
+
+
+      {/* <button onClick={() => addTask()}>Agregar Tarea</button> */}
     </>
-  )
+  );
 }
