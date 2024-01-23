6134caf2e67ab5f63f4b01835a5498516adac75e
NehuenCarballo <nehuencar123@gmail.com>
2024-01-22 21:40:04 -0300

    useFetch

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
-import {Formulario} from "./EjercicioFormulario/Formulario.jsx";
 import  "./index.css"
+import { FetchWithHook } from "./Fetch-useEffect/FetchWithHook.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Formulario  />
+    <FetchWithHook  />
   </React.StrictMode>
 );
